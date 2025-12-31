38e92a42b471da2eaffaa0a4a84775c8a5acfbb0
Sirajis Salekin Sajon <sirajis.sajon@selisegroup.com>
2025-12-31 17:48:47 +0600

    Rounded corners work for planes now

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,10 @@ class MyApp extends StatelessWidget {
           child: Container(
             clipBehavior: .antiAlias,
             decoration: BoxDecoration(),
-            child: CustomPaint(painter: PaintBarGraph(), size: Size(200, 200)),
+            child: CustomPaint(
+              painter: PaintBarGraph(income: 0, expense: 0),
+              size: Size(200, 200),
+            ),
           ),
         ),
       ),
@@ -46,7 +49,10 @@ class MyApp extends StatelessWidget {
 }
 
 class PaintBarGraph extends CustomPainter {
-  PaintBarGraph();
+  PaintBarGraph({required this.income, required this.expense});
+
+  double expense;
+  double income;
 
   final backGroundPaint = Paint()
     ..color = .fromRGBO(0, 0, 0, 0.05)
@@ -82,23 +88,28 @@ class PaintBarGraph extends CustomPainter {
     //   _getAngledOffset(groundOriginPoint, widthMidPoint, (180 + viewAngle)),
     //   groundPainter,
     // );
-    List<Offset> topSidePathPoints = getPointsForPlanePolygon(
-      topPlaneOriginPoint,
-      widthMidPoint,
-      viewAngle,
+
+    const arcPercentage = 10.0;
+    final topSidePathPoints = getPointsForPlanePolygon(
+      origin: topPlaneOriginPoint,
+      xProjectionLength: widthMidPoint,
+      angleOfPoint1And2: viewAngle,
+      arcPercentage: arcPercentage,
     );
 
     final leftSidePathPoints = getPointsForLeftSidePolygon(
-      topPlaneOriginPoint,
-      widthMidPoint,
-      viewAngle,
-      40,
+      origin: topPlaneOriginPoint,
+      xProjectionLength: widthMidPoint,
+      angleOfPoint1And2: viewAngle,
+      height: 40,
+      arcPercentage: arcPercentage,
     );
     final rightSidePathPoints = getPointsForRightSidePolygon(
-      topPlaneOriginPoint,
-      widthMidPoint,
-      viewAngle,
-      40,
+      origin: topPlaneOriginPoint,
+      xProjectionLength: widthMidPoint,
+      angleOfPoint1And2: viewAngle,
+      height: 40,
+      arcPercentage: arcPercentage,
     );
 
     final topPlanePath = _getPathForPoints(topSidePathPoints);
@@ -107,21 +118,21 @@ class PaintBarGraph extends CustomPainter {
 
     final planePainter = Paint()
       ..shader = ui.Gradient.linear(
-        topSidePathPoints[3],
-        topSidePathPoints[1],
+        topSidePathPoints[3].$1,
+        topSidePathPoints[1].$1,
         [Color(0xFFE5D5FF), Color(0xFFBDCBFD)],
       )
       ..style = .fill;
     canvas.drawPath(topPlanePath, planePainter);
     planePainter.shader = ui.Gradient.linear(
-      leftSidePathPoints[1],
-      leftSidePathPoints[0],
+      leftSidePathPoints[1].$1,
+      leftSidePathPoints[0].$1,
       [Color(0xFF93E3FC), Color(0xFF93AAFC)],
     );
     canvas.drawPath(leftSidePath, planePainter);
     planePainter.shader = ui.Gradient.linear(
-      leftSidePathPoints[1],
-      leftSidePathPoints[0],
+      leftSidePathPoints[1].$1,
+      leftSidePathPoints[0].$1,
       [Color(0xFF5844D7), Color(0xFF6580E1)],
     );
     canvas.drawPath(rightSidePath, planePainter);
@@ -134,12 +145,31 @@ class PaintBarGraph extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
-Path _getPathForPoints(List<Offset> points) {
+enum _PathType { arc, line }
+
+Path _getPathForPoints(List<(Offset, _PathType, Offset?)> points) {
   final path = Path();
   if (points.isNotEmpty) {
-    path.moveTo(points.first.dx, points.first.dy);
+    final (firstPoint, _, _) = points.first; //Ignore the type for first point
+    path.moveTo(firstPoint.dx, firstPoint.dy);
     for (int i = 1; i < points.length; i++) {
-      path.lineTo(points[i].dx, points[i].dy);
+      final (point, type, controlPoint) = points[i];
+      switch (type) {
+        case _PathType.arc:
+          if (controlPoint != null) {
+            path.quadraticBezierTo(
+              controlPoint.dx,
+              controlPoint.dy,
+              point.dx,
+              point.dy,
+            );
+          } else {
+            path.lineTo(point.dx, point.dy);
+          }
+          break;
+        case _PathType.line:
+          path.lineTo(point.dx, point.dy);
+      }
     }
     path.close();
   }
@@ -159,52 +189,131 @@ Offset _getAngledOffset(Offset origin, double oppositeLength, double angle) {
   );
 }
 
-List<Offset> getPointsForPlanePolygon(
-  Offset origin,
-  double xProjectionLength,
-  double angleOfPoint1And2,
-) {
-  return [
-    origin,
-    _getAngledOffset(origin, xProjectionLength, angleOfPoint1And2),
-    origin -
-        Offset(
-          0,
-          2 * math.tan(_degToRadian(angleOfPoint1And2)) * xProjectionLength,
-        ),
-    _getAngledOffset(origin, xProjectionLength, 180 - angleOfPoint1And2),
-    origin,
-  ];
-}
+List<(Offset, _PathType, Offset?)> getPointsForPlanePolygon({
+  required Offset origin,
+  required double xProjectionLength,
+  required double angleOfPoint1And2,
+  required double arcPercentage,
+}) {
+  final arcDiameter = xProjectionLength * (arcPercentage / 100);
+  final radianAngle = _degToRadian(angleOfPoint1And2);
 
-List<Offset> getPointsForLeftSidePolygon(
-  Offset origin,
-  double xProjectionLength,
-  double angleOfPoint1And2,
-  double height,
-) {
-  final secondPoint = _getAngledOffset(
-    origin,
-    xProjectionLength,
-    180 - angleOfPoint1And2,
-  );
-  final thirdPoint = secondPoint + Offset(0, height);
-  final fourthPoint = origin + Offset(0, height);
-  return [origin, secondPoint, thirdPoint, fourthPoint, origin];
-}
-
-List<Offset> getPointsForRightSidePolygon(
-  Offset origin,
-  double xProjectionLength,
-  double angleOfPoint1And2,
-  double height,
-) {
-  final secondPoint = _getAngledOffset(
+  // Original sharp corners (used as control points for rounded arcs)
+  final rightCorner = _getAngledOffset(
     origin,
     xProjectionLength,
     angleOfPoint1And2,
   );
+  final leftCorner = _getAngledOffset(
+    origin,
+    xProjectionLength,
+    180 - angleOfPoint1And2,
+  );
+  final topCorner = Offset(
+    origin.dx,
+    origin.dy - 2 * math.tan(radianAngle) * xProjectionLength,
+  );
+  final bottomCorner = origin;
+
+  // Points shifted inward from corners for the rounded shape
+  final shiftedOrigin = _getAngledOffset(
+    origin,
+    arcDiameter,
+    angleOfPoint1And2,
+  );
+  final secondPoint = _getAngledOffset(
+    origin,
+    xProjectionLength - arcDiameter,
+    angleOfPoint1And2,
+  );
+  final thirdPoint =
+      secondPoint - Offset(0, 2 * math.tan(radianAngle) * arcDiameter);
+  final fourthPoint =
+      shiftedOrigin -
+      Offset(0, 2 * math.tan(radianAngle) * (xProjectionLength - arcDiameter));
+  final eigthPoint = _getAngledOffset(
+    origin,
+    arcDiameter,
+    180 - angleOfPoint1And2,
+  );
+  final seventhPoint = _getAngledOffset(
+    origin,
+    xProjectionLength - arcDiameter,
+    180 - angleOfPoint1And2,
+  );
+  final fifthPoint =
+      eigthPoint -
+      Offset(0, 2 * math.tan(radianAngle) * (xProjectionLength - arcDiameter));
+  final sixthPoint =
+      seventhPoint - Offset(0, 2 * math.tan(radianAngle) * arcDiameter);
+  return [
+    (shiftedOrigin, .line, null),
+    (secondPoint, .line, null),
+    (thirdPoint, .arc, rightCorner), // Arc around right corner
+    (fourthPoint, .line, null),
+    (fifthPoint, .arc, topCorner), // Arc around top corner
+    (sixthPoint, .line, null),
+    (seventhPoint, .arc, leftCorner), // Arc around left corner
+    (eigthPoint, .line, null),
+    (shiftedOrigin, .arc, bottomCorner), // Arc around bottom corner
+  ];
+}
+
+List<(Offset, _PathType, Offset?)> getPointsForLeftSidePolygon({
+  required Offset origin,
+  required double xProjectionLength,
+  required double angleOfPoint1And2,
+  required double height,
+  required double arcPercentage,
+}) {
+  final arcDiameter = xProjectionLength * (arcPercentage / 100);
+
+  final shiftedOrigin = _getAngledOffset(
+    origin,
+    arcDiameter,
+    180 - angleOfPoint1And2,
+  );
+  final secondPoint = _getAngledOffset(
+    origin,
+    xProjectionLength - arcDiameter,
+    180 - angleOfPoint1And2,
+  );
   final thirdPoint = secondPoint + Offset(0, height);
-  final fourthPoint = origin + Offset(0, height);
-  return [origin, secondPoint, thirdPoint, fourthPoint, origin];
+  final fourthPoint = shiftedOrigin + Offset(0, height);
+  return [
+    (shiftedOrigin, .line, null),
+    (secondPoint, .line, null),
+    (thirdPoint, .line, null),
+    (fourthPoint, .line, null),
+    (shiftedOrigin, .line, null),
+  ];
+}
+
+List<(Offset, _PathType, Offset?)> getPointsForRightSidePolygon({
+  required Offset origin,
+  required double xProjectionLength,
+  required double angleOfPoint1And2,
+  required double height,
+  required double arcPercentage,
+}) {
+  final arcDiameter = xProjectionLength * (arcPercentage / 100);
+  final shiftedOrigin = _getAngledOffset(
+    origin,
+    arcDiameter,
+    angleOfPoint1And2,
+  );
+  final secondPoint = _getAngledOffset(
+    origin,
+    xProjectionLength - arcDiameter,
+    angleOfPoint1And2,
+  );
+  final thirdPoint = secondPoint + Offset(0, height);
+  final fourthPoint = shiftedOrigin + Offset(0, height);
+  return [
+    (shiftedOrigin, .line, null),
+    (secondPoint, .line, null),
+    (thirdPoint, .line, null),
+    (fourthPoint, .line, null),
+    (shiftedOrigin, .line, null),
+  ];
 }
